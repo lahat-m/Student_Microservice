17733a7e20e868e0127183277f529e78e0e9d1da
lahat-m <lahatm.apach@gmail.com>
2025-06-21 17:54:32 +0300

    Refactor StudentController: update endpoint changed from PUT to PATCH and adjusted imports and annotations

--- src/main/java/com/mmaas/StudentMicroservice/controller/StudentController.java
+++ src/main/java/com/mmaas/StudentMicroservice/controller/StudentController.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,14 +16,15 @@ import com.mmaas.StudentMicroservice.dto.StudentDTO;
 import com.mmaas.StudentMicroservice.model.Student;
 import com.mmaas.StudentMicroservice.service.StudentService;
 
-import lombok.RequiredArgsConstructor;
-
-@RestController
-@RequestMapping("/api/students")
-@RequiredArgsConstructor
+@RestController // tell Springs to create and manage instance of StudentCOntroller
+@RequestMapping("/api/v1/students")
 public class StudentController {
 
-    private final StudentService studentService;
+    private final StudentService studentService; // dependency
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
     @PostMapping
     public ResponseEntity<Student> createStudent(@RequestBody StudentDTO dto) {
@@ -40,7 +41,7 @@ public class StudentController {
         return ResponseEntity.ok(studentService.getStudentById(id));
     }
 
-    @PutMapping("/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<Student> update(@PathVariable String id, @RequestBody StudentDTO dto) {
         return ResponseEntity.ok(studentService.updateStudent(id, dto));
     }
