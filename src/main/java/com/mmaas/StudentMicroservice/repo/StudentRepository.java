3b5b3767e8e5f4acab1fdfc7c5367218df84cdd5
lahat-m <lahatm.apach@gmail.com>
2025-06-21 23:02:37 +0300

    Add student query logic by email

--- src/main/java/com/mmaas/StudentMicroservice/repo/StudentRepository.java
+++ src/main/java/com/mmaas/StudentMicroservice/repo/StudentRepository.java
@@ -1,8 +1,12 @@
 package com.mmaas.StudentMicroservice.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.mmaas.StudentMicroservice.model.Student;
 
+
 public interface StudentRepository extends MongoRepository<Student, String> {
+    Optional<Student> findByEmail(String email);
 }
